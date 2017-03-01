ddc2e9f057aa9eb2d2f0cb5b495eed695810488c
benedictevc76 <diverse563@gmail.com>
2017-03-01 12:40:53 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,25 @@
+/*** Modals ***/
+$('#faqLink').on('click', function(event){
+    $('#faqModal').modal({
+        keyboard: false,
+        backdrop: 'static'
+    });
+});
+
+$('#juleLink').on('click', function(event){
+    $('#juleModal').modal({
+        keyboard: false,
+        backdrop: 'static'
+    });
+});
+
+$('#betingelserLink').on('click', function(event){
+    $('#betingelserModal').modal({
+        keyboard: false,
+        backdrop: 'static'
+    });
+});
+
+
+
+
